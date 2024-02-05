bc7c8e292cab4d3b0c889d2425518b1d9117fb50
enbipaipu <s1f102300122@iniad.org>
2024-02-05 18:56:05 +0900

    add

--- ABC/ABC339/D.py
+++ ABC/ABC339/D.py
@@ -1,3 +1,25 @@
-n = int(input())
+Vec = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
+n = int(input())
 s = [input() for _ in range(n)]
+
+grid = {y: {u: t for u, t in enumerate(x)} for y, x in enumerate(s)}
+
+
+def move_pos(position, y, x):
+    new_position = (position[0] + y, position[1] + x)
+
+    if grid[new_position[0]][new_position[1]] == "#":
+        return position
+    return new_position
+
+
+player = [(y, x) for y, X in enumerate(s) for x, T in enumerate(X) if T == "p"]
+
+player1, player2 = player
+
+
+for X in grid:
+    print(grid[X])
+
+print(player1, player2)
--- ABC/ABC339/test.py
+++ ABC/ABC339/test.py
@@ -1,3 +1,7 @@
-dic = {y: {x: x**2 + y for x in range(1, 5)} for y in range(1, 4)}
+# dic = {y: {x: x**2 + y for x in range(1, 5)} for y in range(1, 4)}
 
-[print(dic[s][x]) for s in range(1, 4) for x in dic[s]]
+# [print(dic[s][x]) for s in range(1, 4) for x in dic[s]]
+N = 4
+dp = [[-1] * (N * N) for _ in range(N * N)]
+
+print(dp)
