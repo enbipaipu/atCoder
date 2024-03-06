eb7d7c26593c4113b59a7f442c112359e9cc6e05
enbipaipu <s1f102300122@iniad.org>
2024-03-06 17:49:05 +0900

    add A30.py

---
+++ 演習問題/first/5.mathematical_problems/A30.py
@@ -0,0 +1,16 @@
+n, r = map(int, input().split())
+M = 1000000007
+
+# 手順 1：分子を求める
+a = 1
+for i in range(1, n + 1):
+    a = (a * i) % M
+
+# 手順 2：分母を求める
+b = 1
+for i in range(1, r + 1):
+    b = (b * i) % M
+for i in range(1, n - r + 1):
+    b = (b * i) % M
+
+print(a * pow(b, M - 2, M) % M)
