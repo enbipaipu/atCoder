1bf8cf1d300ef080cfe2c61ccc475cb7bd62ec50
enbipaipu <s1f102300122@iniad.org>
2024-04-14 23:47:35 +0900

    add a39

--- 演習問題/test.py
+++ 演習問題/test.py
@@ -1,34 +1,11 @@
-# dic = {1: "a", 2: "b", 3: None}
-# lis = [4, 5, 6, 7, 3]
-# value = "abcdef"
-# dic1 = {s: t for s, t in enumerate(value)}
-# bol = True if value == "abcdea" else False
-# se = set(["a", "a", "b"])
-# A = [[None] * 3 for _ in range(4)]
-# B = [0, 2, 3]
-# print(sum(B))
-
-import time
-
-start = time.time()
-original_list = [1, 2, 3, 4, 5]
-doubled_list = [x * 2 for x in original_list]
-print(doubled_list)
-finish = time.time()
-print(finish - start)
-
-start2 = time.time()
-original_list = [1, 2, 3, 4, 5]
-doubled_list = list(map(lambda x: x * 2, original_list))
-print(doubled_list)
-finish2 = time.time()
-print(finish2 - start2)
-
-start3 = time.time()
-original_list = [1, 2, 3, 4, 5]
-doubled_list = []
-for x in original_list:
-    doubled_list.append(x * 2)
-print(doubled_list)
-finish3 = time.time()
-print(finish3 - start3)
+dic = {1: "a", 2: "b", 3: None}
+lis = [4, 5, 6, 7, 3]
+value = "abcdef"
+dic1 = {s: t for s, t in enumerate(value)}
+bol = True if value == "abcdea" else False
+se = set(["a", "a", "b"])
+A = [[None] * 3 for _ in range(4)]
+B = [0, 2, 3]
+lis2 = [[4, 2, 3], [1, 2, 3], [1, 2, 3]]
+lis2.sort()
+print(lis2)
