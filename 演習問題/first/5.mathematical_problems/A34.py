812e1e10502a246cbc3e0050e2ab586bb5b26381
enbipaipu <s1f102300122@iniad.org>
2024-04-14 17:53:04 +0900

    add A34

---
+++ 演習問題/first/5.mathematical_problems/A34.py
@@ -0,0 +1,29 @@
+N, X, Y = map(int, input().split())
+
+A = list(map(int, input().split()))
+
+
+grundy = [None] * 100001
+
+for i in range(100001):
+    Transit = [False, False, False]
+    if i >= X:
+        Transit[grundy[i - X]] = True
+    if i >= Y:
+        Transit[grundy[i - Y]] = True
+
+    if Transit[0] is False:
+        grundy[i] = 0
+    elif Transit[i] is False:
+        grundy[i] = 1
+    else:
+        grundy[i] = 2
+
+# 出力
+XOR_Sum = 0
+for i in range(N):
+    XOR_Sum = XOR_Sum ^ grundy[A[i]]
+if XOR_Sum >= 1:
+    print("First")
+else:
+    print("Second")
