9237b15f2ec1e163efe2a7cf0f9846f3c1d992cf
enbipaipu <s1f102300122@iniad.org>
2024-04-16 23:09:25 +0900

    add a42

--- 演習問題/test.py
+++ 演習問題/test.py
@@ -8,4 +8,6 @@ A = [[None] * 3 for _ in range(4)]
 B = [0, 2, 3]
 lis2 = [[4, 2, 3], [1, 2, 3], [1, 2, 3]]
 lis2.sort()
-print(lis2)
+dic2 = {"1": ["c", "d"], "2": ["a", "b"]}
+lis3 = sorted([x for x in dic2.values()])
+print(lis3)
