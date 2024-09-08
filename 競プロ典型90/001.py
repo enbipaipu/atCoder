7c38a049a4f20281e072178eda9f97c6dc490005
ishii <s1f102300122@iniad.org>
2024-09-08 20:42:34 +0900

    001

---
+++ 競プロ典型90/001.py
@@ -0,0 +1,25 @@
+## not
+N, L = map(int, input().split())
+K = int(input())
+A = list(map(int, input().split()))
+
+
+def check(x):
+    num = 0
+    pre = 0
+    for i in range(N):
+        if A[i]-pre >= x:
+            num+=1
+            pre=A[i]
+    if L-pre>=x:
+        num+=1
+
+left, right = -1, L+1
+
+while right - left > 1:
+    mid = (left + right)//2
+    if check(mid):
+        left = mid
+    else:
+        right = mid
+print(left)
